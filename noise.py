f756a9d0e67faec95f311923e8515bebc4c4ea07
Ra1issa <ra1issa@bu.edu>
2018-12-01 21:22:47 -0500

    add more noise generating mechanisms and fix gaussian

--- noise.py
+++ noise.py
@@ -1,16 +1,40 @@
 import scipy.stats
 import math
+
 import numpy as np
 from scipy.optimize import fsolve
 
 # based on work in: https://eprint.iacr.org/2016/943.pdf
-
 def poisson_noise(lambda_, c, n):
-
     val = float(lambda_ + c*math.sqrt(lambda_))
     epsilon =  math.log(val/lambda_)
-    delta = math.exp(-lambda_)*math.exp(val)*(lambda_**val)/(val**val)
+
+    try:
+        op1 = -lambda_
+        op2 = 1+math.log(lambda_)
+        op2 = op2*val
+        op3 = math.log(val)*val
+
+        f2 = op1 + op2 - op3
+        delta = math.exp(f2)
+
+    except:
+        print("delta too small, setting it to 0")
+        delta = 0
 
     print("epsilon:", epsilon," delta:",delta)
 
     return np.random.poisson(lambda_, n)
+
+
+def laplace_noise(epsilon, n):
+    return np.random.laplace(scale = 1/epsilon, size = n)
+
+def geometric_noise(epsilon, n):
+    return scipy.stats.dlaplace.rvs(epsilon, size = n)
+
+def gaussian_noise(epsilon, delta, n):
+    #not sure about GS_2(f)
+    global_sensitivity_2 = 1
+    sigma = sqrt(2*math.log(1/delta))/epsilon*global_sensitivity_2
+    return np.random.normal(0, sigma, n)
