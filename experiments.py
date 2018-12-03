55c478f4cd22cc74539168c3dfbfe355d4d7f98a
Sarah Scheffler <sscheffler@hmc.edu>
2018-12-03 13:37:10 -0500

    experiments now run all apportionment algorithms and noise algorithms implemented so far

--- experiments.py
+++ experiments.py
@@ -1,15 +1,22 @@
 import csv
 import numpy as np
 from states import STATE_ORDER, ABBR_TO_NAME, NAME_TO_ABBR
-from apportionment import huntington_hill
+from apportionment import huntington_hill, webster, dean, hamilton, lowndes, jefferson, adam
+from noise import poisson_noise, laplace_noise, geometric_noise, gaussian_noise
 
 EPSILONS = [10**x for x in range(-1, -6, -1)]
+DELTAS = [10**x for x in range(-4, -20, -4)]
+CS = range(1, 11, 2)
 REPS = 100
 
 POPULATIONS_FILE = "census_data/historical_populations.csv"
 APPORTIONMENT_FILE = "census_data/house_apportionments.csv"
 OUTPUT_FOLDER = "results"
-VERBOSE = True
+VERBOSE = False
+
+apportionments = {"hh": huntington_hill, "webster": webster, "dean": dean, "hamilton": hamilton, "lowndes": lowndes,
+        "jefferson": jefferson, "adam": adam}
+mechanisms = { "poisson": poisson_noise, "laplace": laplace_noise, "geometric": geometric_noise, "gaussian": gaussian_noise}
 
 #########################################################
 # Parsing functions
@@ -80,14 +87,17 @@ def parse_historical_seats_apportioned(filename=APPORTIONMENT_FILE):
 # Experiments
 #########################################################
 
-# TEMPORARY NOISE FUNCTION
-def laplace_noise(dims, epsilon):
-    # 1/epsilon because GS of count is 1
-    return np.random.laplace(scale= 1/epsilon, size = dims)
-
-def run_experiment():
+def run_experiment(apportion_alg_name, count_mech_name):
     # Writes a function that returns the changes that must be made TO THE TRUE APPORTIONMENT in order to equal the
     # NOISY RESULT
+
+    print("++++++++++++++++++++++++++++++++++++")
+    print("apportionment algorithm: ", apportion_alg_name)
+    print("count DP mechanism: ", count_mech_name)
+    print("++++++++++++++++++++++++++++++++++++")
+
+    apportionment_alg = apportionments[apportion_alg_name]
+    count_mechanism = mechanisms[count_mech_name]
 
     census_years, total_us_pop, state_pops = parse_historical_populations(POPULATIONS_FILE)
     census_years, total_seats_apportioned, state_seats_apportioned = parse_historical_seats_apportioned(APPORTIONMENT_FILE)
@@ -99,36 +109,43 @@ def run_experiment():
         true_population = total_us_pop[year]
         true_state_pop = state_pops[year]
         true_seats = total_seats_apportioned[year]
-        true_answer = huntington_hill(true_population, true_state_pop, true_seats)
+        true_answer = apportionment_alg(true_population, true_state_pop, true_seats)
 
-        with open(OUTPUT_FOLDER + "/" + str(year) + ".csv", 'w') as csvfile:
+        output_file = OUTPUT_FOLDER + "/" + apportion_alg_name + "/" + count_mech_name + "/" + str(year) + ".csv"
+        with open(output_file, 'w') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(["epsilon", "rep"] + STATE_ORDER)
+            writer.writerow(["apportionment","mechanism", "epsilon", "rep"] + STATE_ORDER)
 
             for epsilon in EPSILONS:
                 print("  epsilon: ", str(epsilon))
                 if VERBOSE: print("---------------------------------")
 
-                for rep in range(REPS):
+                deltas = DELTAS if count_mech_name == "gaussian" else [None]
+                for delta in deltas:
+                    print("    delta: ", str(delta))
+                    if VERBOSE: print("  ---------------------------------")
 
-                    noises = laplace_noise(len(true_state_pop), epsilon)
-                    population = true_population + sum(noises)
-                    state_pop = dict()
-                    for (state, i) in zip(true_state_pop.keys(), range(len(true_state_pop))):
-                        state_pop[state] = true_state_pop[state] + noises[i] if true_state_pop[state] is not None else None
-                    answer = huntington_hill(population, state_pop, true_seats)
+                    for rep in range(REPS):
+                        noises = count_mechanism(epsilon, delta, len(true_state_pop)) if count_mech_name == "gaussian" else count_mechanism(epsilon, len(true_state_pop))
+                        population = true_population + sum(noises)
+                        state_pop = dict()
+                        for (state, i) in zip(true_state_pop.keys(), range(len(true_state_pop))):
+                            state_pop[state] = true_state_pop[state] + noises[i] if true_state_pop[state] is not None else None
+                        answer = apportionment_alg(population, state_pop, true_seats)
 
-                    if true_answer != answer:
-                        if VERBOSE: print("epsilon: %f, year: %d, rep: %d, Different" % (epsilon, year, rep))
-                        for state in true_answer:
-                            if true_answer[state] != answer[state]:
-                                diff = answer[state] - true_answer[state]
-                                if VERBOSE: print("    ", state, "+" if diff > 0 else " " if diff == 0 else "", diff)
-                    else:
-                        if VERBOSE: print("epsilon: %f, year: %d, rep: %d, Same" % (epsilon, year, rep))
+                        if true_answer != answer:
+                            if VERBOSE: print("alg: %s, mech: %s, epsilon: %f, year: %d, rep: %d, Different" %
+                                    (apportion_alg_name, count_mech_name, epsilon, year, rep))
+                            for state in true_answer:
+                                if true_answer[state] != answer[state]:
+                                    diff = answer[state] - true_answer[state]
+                                    if VERBOSE: print("    ", state, "+" if diff > 0 else " " if diff == 0 else "", diff)
+                        else:
+                            if VERBOSE: print("alg: %s, mech: %s, epsilon: %f, year: %d, rep: %d, Same" %
+                                    (apportion_alg_name, count_mech_name, epsilon, year, rep))
 
-                    writer.writerow([epsilon, rep] + [answer[st] - true_answer[st] for st in filter(lambda st: true_answer[st] is
-                        not None, STATE_ORDER)])
+                        writer.writerow([apportion_alg_name, count_mech_name, epsilon, rep] + [answer[st] - true_answer[st] for st in filter(lambda st: true_answer[st] is
+                            not None, STATE_ORDER)])
     print("Done!  Results are in ", OUTPUT_FOLDER)
 
 
@@ -136,7 +153,11 @@ if __name__ == '__main__':
     census_years, total_us_pop, state_pops = parse_historical_populations(POPULATIONS_FILE)
     census_years, total_seats_apportioned, state_seats_apportioned = parse_historical_seats_apportioned(APPORTIONMENT_FILE)
 
-    run_experiment()
+    for aa in apportionments.keys():
+        for cm in mechanisms.keys():
+            if cm == "poisson":
+                continue # skip for now
+            run_experiment(aa, cm)
 
     #print(census_years)
     #print(total_seats_apportioned)
